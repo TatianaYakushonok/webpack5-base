15a7334a02be1b5e54d6e2d4f7d9e4da47129061
TatianaYakushonok <yakush2@yandex.ru>
2022-12-23 03:04:47 +0700

    feat: add filemanager-webpack-plugin (Fri, 23 Dec 2022 03:04:46 +0700)

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.js'),
@@ -9,8 +10,15 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-        template: path.join(__dirname, 'src', 'template.html'),
-        filename: 'index.html',
+            template: path.join(__dirname, 'src', 'template.html'),
+            filename: 'index.html',
+        }),
+        new FileManagerPlugin({
+            events: {
+            onStart: {
+                delete: ['dist'],
+                },
+            },
         }),
     ],
     devServer: {
